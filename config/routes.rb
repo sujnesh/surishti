79cd80d350a3a42b9b86f1cb156d27a96a237acf
Sujnesh Mishra <sujnesh@cogoport.com>
2020-02-18 16:19:53 +0530

    devise signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end
 
   root to: 'pages#index'
