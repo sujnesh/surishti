131a62722fefe137991aec823316487c0f804c6f
Sujnesh Mishra <sujnesh@cogoport.com>
2020-02-24 18:24:41 +0530

    bug fix

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -17,7 +17,7 @@ module PostsHelper
   end
 
   def no_posts_partial_path(posts)
-    posts.empty? ? 'posts/shared/no_posts' : 'shared/empty_partial'
+    posts.empty? ? 'shared/no_posts' : 'shared/empty_partial'
   end
 
   def post_format_partial_path
